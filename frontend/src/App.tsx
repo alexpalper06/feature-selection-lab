ea63804bb6ccd7ee7bb0943abe721c7d759acbe9
alexpalper06 <alex.palacios@goumh.umh.es>
2026-05-24 17:09:21 +0200

    Fix some missing tabulation in some pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,19 @@
-// src/App.tsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import DatasetListPage from './pages/DatasetListPage';
 import DatasetUploadPage from './pages/DatasetUploadPage';
 import DatasetPanelPage from "./pages/DatasetPanelPage.tsx";
 
 export default function App(): React.ReactElement {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/datasets" element={<DatasetListPage />} />
-        <Route path="/datasets/upload" element={<DatasetUploadPage />} />
-        <Route path="/datasets/:datasetId" element={<DatasetPanelPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DashboardPage/>}/>
+                <Route path="/datasets" element={<DatasetListPage/>}/>
+                <Route path="/datasets/upload" element={<DatasetUploadPage/>}/>
+                <Route path="/datasets/:datasetId" element={<DatasetPanelPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
